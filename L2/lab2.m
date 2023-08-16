90c1c710c0f3d3948aa018684263e068547d3b53
GabrielRalph <gltralph@gmail.com>
2023-08-16 15:26:42 +1000

    data

--- L2/lab2.m
+++ L2/lab2.m
@@ -1,27 +1,39 @@
 La = ["Sound level meter(dB)", "Android App (dB)", "iPhone App (dB)"];
-A = [
-    63.9, 52.1,53;
-    73.9, 60.8, 55.9;
-    83.4, 69.2, 62.9;
-    94.8, 75.4, 65;
-];
 
+tble = [];
 Po = 1.3;
-A2 = [65.4, 52.1, 53;
-      74.8, 60.8, 55.9;
-      84.8, 69.2, 62.9;
-      94.8, 74.4, 65];
-A2d = A2(2:end, :) - A2(1:(end-1), :);
-A2p = 10.^(A2d/20) * Po;
-A2pm = mean(A2p);
-A2dm = 20*log10(A2pm/Po);
+data = {[
+    65.4, 52.1, 53;
+    74.8, 60.8, 55.9;
+    84.8, 69.2, 62.9;
+    94.8, 74.4, 65
+],[
+    63.9, 50.8,50.4;
+    73.9, 55.9, 60.9;
+    83.4, 64.6, 58;
+]; "1000Hz", "400Hz"};
 
-
-
-Lab2 = ["Sound level meter(dB)", "Differnece (dB)", "Android App (dB)", "Difference (dB)", "iPhone App (dB)", "Difference (dB)";
-        A2(:, 1), ["-";A2d(:, 1)], A2(:, 2), ["-";A2d(:, 2)], A2(:, 3), ["-";A2d(:, 3)];
-        "Mean",A2dm(1), "Mean", A2dm(2), "Mean", A2dm(3)];
-writematrix(Lab2,"table1.csv");
+for i = 1:2
+    A = data{1, i};
+    % Differences
+    Adif = A(2:end, :) - A(1:(end-1), :);
+    % To Sound Preasure
+    Adifp = 10.^(Adif/20) * Po;
+    % Average of sound preasure
+    Adiffpm = mean(Adifp);
+    % Convert Averages back to dB
+    Adiffpd = 20*log10(Adiffpm/Po);
+    
+    Adiffpm = arrayfun(@(x) sprintf("%.2f", x), Adiffpm);
+    Adiffpd = arrayfun(@(x) sprintf("%.2f", x), Adiffpd);
+    
+    tble = cat(1, tble, [" ", "", "", "", "", "", data{2, i};
+            " ","Sound level meter(dB)", "Differnece (dB)", "Android App (dB)", "Difference (dB)", "iPhone App (dB)", "Difference (dB)";
+            repmat(" ", length(A(:,1)), 1), A(:, 1), ["-";Adif(:, 1)], A(:, 2), ["-";Adif(:, 2)], A(:, 3), ["-";Adif(:, 3)];
+            "Average (P)", " " ,Adiffpm(1), " ", Adiffpm(2), " ", Adiffpm(3);
+            "Average (dB)", " " ,Adiffpd(1), " ", Adiffpd(2), " ", Adiffpd(3)]);
+end
+writematrix(tble,"table1.csv");
 
 %%
 
@@ -31,30 +43,38 @@ B = ["Apple Airpods Gen 3",                                 "EDM",          85.6
      "Sony closed back headphones",                         "Pop",          70,   75;
      "Audio technica in ear buds",                          "Metal",        80,   0];
 
-Lc = ["Range(m)", "R1", "R2"];
+%%
+
+Lc = ["Range(m)", "Test 1", "Test 2"];
 C = [0.1,     115.6,  99;
      2.1,     83.1,   83.4;
      9.1,    82.1,   81.3;
      21.1,    72.8,   75.1;
-     27.1,    74.2,   71.7]
+     27.1,    74.2,   71.7];
 
 
 
 avgC = mean(C(:, 2:3), 1);
 logCx = log(C(:, 1));
-% p = polyfit(logCx, avgC, 2);
 
-% lx = linspace(0.1, 27.1, 5);
-% ly = polyval(p, log(lx));
-% str2num(B(:, 3))
-
+% Cp = 10.^(C(1, 2:end)/20);
+% Ip = (C(1,1)./C(:, 1)).^2 * Cp;
+% Ipd = 20*log10(Ip)
+% 
+% semilogx(C(:, 1), Ipd(:, 2), C(:, 1), Ipd(:, 1)); %, lx, ly);
+% hold on;
 semilogx(C(:, 1), C(:, 2), C(:, 1), C(:, 3)); %, lx, ly);
+
+
+
 xlabel("Distance (m)");
 ylabel("SPL (dB)");
-legend(["Repeat 1", "Repeat 2"]);
+legend(Lc(2:end));
+title("Sound Pressure Level vs Range");
 grid on;
 
-
+saveas(gcf, "plot1", "svg");
+writematrix([Lc; C],"table2.csv");
 %%
 audiodevreset;
 fs = 44100;
